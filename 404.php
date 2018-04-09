3337a1d2457d1e25a641b2d862bce7ef2f332fa3
frog <verajovanovicbg@hotmail.com>
2018-04-09 19:57:35 +0200

    Fix 404.php page

--- 404.php
+++ 404.php
@@ -1,9 +1,28 @@
 <?php get_header() ?>
 
-<div>
-	<p>
-		Greska, ova stranica nije pronadjena
-	</p>
-</div>
+    <!-- Baner -->
+    <header class="header-page">
+    </header>
+    <!-- End Baner -->
+
+    <!-- Container -->
+    <div>
+    <!-- Main Container -->
+    <div class="container container-main">
+    <div class="row row-main">
+        <!-- Main Content -->
+        <div class="row-article col-md-9 col-sm-8">
+            <article class="text-justify">
+                <p>Greška, ova stranica nije pronađena.</p>
+            </article>
+        </div>
+        <!-- End Main Content -->
+
+        <!-- Sidebar -->
+        <div class="col-md-3 col-sm-4 row-sidebar">
+            <?php get_template_part('sidebar'); ?>
+        </div>
+        <!-- End Sidebar -->
+    </div>
 
 <?php get_footer() ?>
